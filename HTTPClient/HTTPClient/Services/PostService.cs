deff73bc112679cb7f447932974bb59efbb08bf1
Alitor004 <irmaosgomes1302@gmail.com>
2024-08-08 12:44:43 -0300

    feat: Added project

---
+++ HTTPClient/HTTPClient/Services/PostService.cs
@@ -0,0 +1,46 @@
+﻿using HTTPClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HTTPClient.Services
+{
+    public class PostService
+    {
+        private HttpClient httpClient;
+        private List<Post> posts;
+        private JsonSerializerOptions jsonSerializerOptions;
+
+        public PostService()
+        {
+            httpClient = new HttpClient();
+            jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+
+        public async Task<List<Post>> GetPostsAsync() 
+        {
+            Uri uri = new Uri("https://jsonplaceholder.typicode.com/posts");
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(uri);
+                if (response.IsSuccessStatusCode) {
+                    string content = await response.Content.ReadAsStringAsync();
+                    posts = JsonSerializer.Deserialize<List<Post>>(content, jsonSerializerOptions);
+                }
+            }
+            catch
+            {
+
+            }
+            return posts;
+        }
+
+    }
+}
